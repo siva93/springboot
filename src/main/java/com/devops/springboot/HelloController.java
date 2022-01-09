062d91df7c0557f076585849bc4676f29c1e813a
siva93 <b.siva93@gmail.com>
2022-01-09 21:50:32 +0530

    Update HelloController.java

--- src/main/java/com/devops/springboot/HelloController.java
+++ src/main/java/com/devops/springboot/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.hellospring;
+package com.devops.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
