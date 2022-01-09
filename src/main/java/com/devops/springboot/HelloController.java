40a325111af1b4f238c3d89795466a3900b6eaf6
siva93 <b.siva93@gmail.com>
2022-01-09 21:34:45 +0530

    Update HelloController.java

--- src/main/java/com/devops/springboot/HelloController.java
+++ src/main/java/com/devops/springboot/HelloController.java
@@ -6,13 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Azure Kubernetes!";
     }
-    
-	@GetMapping("/")
-	String return1(){
-		return "Hello World - Greetings from Azure Kubernetes!";
-	}
 }
